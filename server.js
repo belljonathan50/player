d9baeb42a44875d475508f401980d17b061617fc
belljonathan50 <belljonathan50@gmail.com>
2022-07-20 23:22:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -63,89 +63,91 @@ app.ws('/', (ws, req) => {
 
     // console.log('message received serverside : '+ message);
 
-    var words = message.split(' ');
-    var first = words[0];
-    var second = words[1];
+    // var words = message.split(' ');
+    // var first = words[0];
+    // var second = words[1];
+
+
     // console.log('first : '+first);
     // console.log('second : '+second + ' typeof : ' + typeof second);
     // var seekValue = parseInt(words[1]);
     // var vid = document.getElementById('vid');
-    
 
 
-
-switch(first) {
-  case "stop":
-    console.log("totoooo");
-    file.set("isPlaying", "false");
-    file.set("seek", 0);
-    console.log(file.get());
-    break;
-
-  case "start":
-    file.set("isPlaying", "true");
-    break;
-
-  case "pause":
-  file.set("isPlaying", "false");
-  break;
-
-  case "seek":
-    file.set("seek", second);
-    console.log(file.get());
-    break;
-
-  case "rate":
-    file.set("rate", second);
-    break;
-
-  case "lag":
-    file.set("lag", second);
-    break;
-
-  case "tune":
-    file.set("tune", second);
-    console.log("changed tune to "+ second)
-    break;
-
-  case "adjS":
-      file.set("adjS", second);
-      console.log("changed adjS to "+ second)
-      break;
-
-  case "adjA":
-        file.set("adjA", second);
-        console.log("changed adjA to "+ second)
-        break;
-
-  case "adjT":
-        file.set("adjT", second);
-        console.log("changed adjT  to "+ second)
-        break;
-
-  case "adjB":
-        file.set("adjB", second);
-        console.log("changed adjB to "+ second)
-        break;
-
-  default:
-    console.log("just a sync");
-    console.log("what's triggering? " + second);
-    // console.log(file.get());
-    // code block
-} 
+//
+//
+// switch(first) {
+//   case "stop":
+//     console.log("totoooo");
+//     file.set("isPlaying", "false");
+//     file.set("seek", 0);
+//     console.log(file.get());
+//     break;
+//
+//   case "start":
+//     file.set("isPlaying", "true");
+//     break;
+//
+//   case "pause":
+//   file.set("isPlaying", "false");
+//   break;
+//
+//   case "seek":
+//     file.set("seek", second);
+//     console.log(file.get());
+//     break;
+//
+//   case "rate":
+//     file.set("rate", second);
+//     break;
+//
+//   case "lag":
+//     file.set("lag", second);
+//     break;
+//
+//   case "tune":
+//     file.set("tune", second);
+//     console.log("changed tune to "+ second)
+//     break;
+//
+//   case "adjS":
+//       file.set("adjS", second);
+//       console.log("changed adjS to "+ second)
+//       break;
+//
+//   case "adjA":
+//         file.set("adjA", second);
+//         console.log("changed adjA to "+ second)
+//         break;
+//
+//   case "adjT":
+//         file.set("adjT", second);
+//         console.log("changed adjT  to "+ second)
+//         break;
+//
+//   case "adjB":
+//         file.set("adjB", second);
+//         console.log("changed adjB to "+ second)
+//         break;
+//
+//   default:
+//     console.log("just a sync");
+//     console.log("what's triggering? " + second);
+//     // console.log(file.get());
+//     // code block
+// }
     connects.forEach(socket => {
-      
+
       socket.send(message + ' '+  JSON.stringify(file));
       // socket.send(file);
-    
+
       // socket.send(file);
     });
 
 
 
   });
-  
+
   ws.on('close', () => {
     connects = connects.filter(conn => {
       return (conn === ws) ? false : true;
@@ -162,5 +164,3 @@ app.use('/timesync', timesyncServer.requestHandler);
 app.listen(app.get('port'), () => {
   console.log('Server listening on port %s', app.get('port'));
 });
-
-
