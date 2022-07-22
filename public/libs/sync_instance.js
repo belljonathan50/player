5d7dad899aa79df60689b98a746e6dd24b52eed8
belljonathan50 <belljonathan50@gmail.com>
2022-07-22 15:36:21 +0200

    Create sync_instance.js

---
+++ public/libs/sync_instance.js
@@ -0,0 +1,16 @@
+  // sync instanciation
+ 
+ var ts = timesync.create({
+  server: '/timesync',
+  interval: 10000
+});
+
+ts.on('change', function (offset) {
+});
+setInterval(function () {
+  var NowHere = new Date(ts.now());
+  var NowNum = NowHere.getTime()
+  console.log(`here: `+NowNum);
+}, 5000);
+
+
