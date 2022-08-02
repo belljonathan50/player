bb0ed44ca648215f27624f11541312aae3c9dd95
belljonathan50 <belljonathan50@gmail.com>
2022-08-02 18:37:23 +0200

    Create sender.js

---
+++ public/libs/sender.js
@@ -0,0 +1,67 @@
+var url = location.origin.replace(/^http/, 'ws');
+var ws = new WebSocket(url);
+
+ws.onmessage = function(msg) { receive (msg.data); };
+
+
+
+
+function refresh(){
+    setInterval(function(){send("dummy;")},10000)
+}
+var client = true;
+
+
+function store (any) {
+    // console.log ("send", msg);
+    window.lastDate = parseFloat(any);
+};
+
+function playAt () {
+    // console.log ("send", msg);
+    start(bach, lastDate)
+}
+
+function send (msg) {
+    // console.log ("send", msg);
+    ws.send (msg);
+    start(bach, lastDate)
+}
+
+function lastTime() {
+    // console.log ("send", msg);
+    var msg = inscore.newMessageM("date");
+    inscore.msgAddF(msg, lastDate);
+    inscore.postMessage("/ITL/scene/pos", msg);
+    console.log("tyuytuytuyt")
+    
+}
+
+function lastStop() {
+    lastDate = 0;  
+}
+
+
+
+
+
+// function storedSpeed(speed) {
+//     var msg = inscore.newMessageM('tempo');
+//     inscore.msgAddF(msg, speed*60);
+//     inscore.postMessage('/ITL/scene/pos', msg);
+// };
+
+function storedDate(date) {
+    var msg = inscore.newMessageM('tempo');
+    inscore.msgAddF(msg, speed*60);
+    inscore.postMessage('/ITL/scene/pos', msg);
+}
+
+
+
+function receive (data) {
+    let n = data.search(';');
+    let msg = data.substring(0, n+1);
+    console.log ("receive", msg);
+    if (client && (msg!="dummy;")) inscore.loadInscore (msg, false);
+}
