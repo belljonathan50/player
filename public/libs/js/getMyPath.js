39da47599d9c18aaf6c35554e1c7443aac649883
belljonathan50 <belljonathan50@gmail.com>
2022-08-04 01:01:59 +0200

    Create getMyPath.js

---
+++ public/libs/js/getMyPath.js
@@ -0,0 +1,11 @@
+var path = window.location.pathname;
+var page = path.split("/").pop();
+
+zval = page.split(".");
+let xval = zval[0];
+
+let thispath = xval;
+
+
+document.write("<center><h1 id='title'></h1> <center>")
+document.getElementById("title").innerHTML = thispath
