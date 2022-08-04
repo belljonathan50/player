f2fc0b7d047b78d7debbb8d789997ec4749f2009
belljonathan50 <belljonathan50@gmail.com>
2022-08-04 16:24:54 +0200

    Create schedulerFunctions.js

---
+++ public/libs/js/conductor/schedulerFunctions.js
@@ -0,0 +1,78 @@
+
+var audioContext = new AudioContext();
+var sched = new WebAudioScheduler({ context: audioContext });
+var masterGain = null;
+
+  function pitch(e) {
+    var t0 = e.playbackTime;
+    var pitch = e.args.note;
+    vox = e.args.voice;
+    getTime();
+    ws.send('sche '+vox+" "+futuredate+" "+pitch);
+  }
+
+  function page(e) {
+    var t0 = e.playbackTime;
+    var npage = thistune + "/" +e.args.npage;
+    vox = e.args.voice;
+    getTime();
+    ws.send('turn '+vox+" "+futuredate+" "+npage);
+  }
+
+
+  function soundPlay(e) {
+    var t0 = e.playbackTime;
+    // var nsound = thistune + "/" +e.args.nsound;
+    vox = e.args.voice;
+    getTime();
+    ws.send('sound '+vox+" "+futuredate);
+    console.log("ms sent")
+  }
+
+  function soundStop(e) {
+    var t0 = e.playbackTime;
+    // var nsound = thistune + "/" +e.args.nsound;
+    vox = e.args.voice;
+    getTime();
+    ws.send('soundStop '+vox+" "+futuredate);
+    console.log("ms sent")
+  }
+
+  function soundSeek(e) {
+    var t0 = e.playbackTime;
+    // var nsound = thistune + "/" +e.args.nsound;
+    vox = e.args.voice;
+    getTime();
+    ws.send('soundSeek '+vox+" "+futuredate);
+    console.log("ms sent")
+  }
+
+sched.on("start", function() {
+  masterGain = audioContext.createGain();
+  masterGain.connect(audioContext.destination);
+});
+
+sched.on("stop", function() {
+  masterGain.disconnect();
+  masterGain = null;
+});
+
+
+
+function stop() {
+  sched.stop(true);
+  
+  ws.send('soundStop sound1 '+futuredate);
+  ws.send('soundStop sound2 '+futuredate);
+  ws.send('soundStop sound3 '+futuredate);
+  ws.send('soundStop sound4 '+futuredate);
+}
+
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "visible") {
+    sched.aheadTime = 0.1;
+  } else {
+    sched.aheadTime = 1.0;
+    sched.process();
+  }
+});
