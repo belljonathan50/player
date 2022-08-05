b63797dc422ffee32a29ee24d1408cace6077751
belljonathan50 <belljonathan50@gmail.com>
2022-08-05 11:28:04 +0200

    Update sync_instance.js

--- public/libs/js/sync_instance.js
+++ public/libs/js/sync_instance.js
@@ -10,12 +10,12 @@ ts.on('change', function (offset) {
 
 
   // reactivate dummy
-setInterval(function () {
-  var NowHere = new Date(ts.now());
-  var NowNum = NowHere.getTime()
-  console.log(`here: `+NowNum);
-  ws.send("dummy");
-}, 5000);
+// setInterval(function () {
+//   var NowHere = new Date(ts.now());
+//   var NowNum = NowHere.getTime()
+//   console.log(`here: `+NowNum);
+//   ws.send("update");
+// }, 5000);
 
 
 
