32bf7534929259ad2e5dae3b219cfcfd002e89cc
belljonathan50 <belljonathan50@gmail.com>
2022-08-11 21:47:52 +0200

    Update InscoreFunctions.js

--- public/libs/js/conductor/InscoreFunctions.js
+++ public/libs/js/conductor/InscoreFunctions.js
@@ -16,7 +16,7 @@ function store (any) {
     // console.log ("send", msg);
     window.lastDate = parseFloat(any);
     ws.send ("seek "+lastDate*4);
-    console.log("lastDate "+lastDate)
+    console.log("lastDate "+lastDate*4)
 };
 
 function lastStop() {
@@ -29,38 +29,54 @@ function lastTime() {
     var msg = inscore.newMessageM("date");
     inscore.msgAddF(msg, lastDate);
     inscore.postMessage("/ITL/scene/pos", msg);
-    console.log("tyuytuytuyt")
-    
 }
 
 
 function playAt() {
+    console.log("thistune "+thistune)
     // when hitting the play button
-    ws.send ("seek "+lastDate);
+    ws.send ("seek "+lastDate*4);
     getTime();
     ws.send('play 1 '+futuredate);
+ 
 
-          // start scheduler at that time
-        // start(bach, lastDate*4);
-          // play video
-   
-    ws.send('video 1 '+futuredate);
-          // play sound
-    
-    console.log("seek "+lastDate)
-  
-}
+    switch (thistune) {
+        
+        case "kunst":
+            start(bach,lastDate*4);
+          break;
+        case "jesu":
+            start(jesu,lastDate*4);
+          break;
+        case "medee":
+            start(medee,lastDate*4);
+          break;
+        case "orfeo":
+            start(orfeo,lastDate*4);
+          break;
+        default:
+          console.log("no tune");
+          break;
+      }
+    // var thistune = Window.thistune;
 
-function pauseVid(){
-    ws.send ("play 0")
-}
+//       switch(thistune) {
+//         case "jesu":
+//           // code block
 
-function send (msg) {
-    // console.log ("send", msg);
-    ws.send (msg);
-    start(bach, lastDate*4)
-}
+// console.log("ljhgkjhgv,khgvkhgvjhgvc");
 
+          
+//           break;
+//         case "orfeo":
+//             console.log("azezdazedazedazed");
+
+//             break; }
+
+
+ }
+
+//  
 // function storedSpeed(speed) {
 //     var msg = inscore.newMessageM('tempo');
 //     inscore.msgAddF(msg, speed*60);
@@ -76,6 +92,6 @@ function storedDate(date) {
 function receive (data) {
     let n = data.search(';');
     let msg = data.substring(0, n+1);
-    console.log ("receive", msg);
+    // console.log ("receive: "+ data);
     if (client && (msg!="dummy;")) inscore.loadInscore (msg, false);
 }
