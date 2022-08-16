868b0183b8c9f76728644e15305013690702115a
belljonathan50 <belljonathan50@gmail.com>
2022-08-16 19:49:44 +0200

    Create getPath.js

---
+++ public/libs/js/getPath.js
@@ -0,0 +1,12 @@
+var path = window.location.pathname;
+var page = path.split("/").pop();
+
+zval = page.split(".");
+let xval = zval[0];
+
+let thispath = xval;
+// let thissoundpath = "/audio/"+thispath +"/"+thistune;
+
+
+// document.write("<center><h1 id='title'></h1> <center>")
+// document.getElementById("title").innerHTML = thispath
